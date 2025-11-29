277ed653dfaa0bf0b1d7c31ce00f66f4cf637792
aciucaru <aciukru@gmail.com>
2025-11-29 16:43:30 +0200

    Added service for 'group membership'

--- src/services/group-membership.service.ts
+++ src/services/group-membership.service.ts
@@ -1,10 +1,15 @@
 import type { AddGroupMemberRequestDTO } from "../dto/AddGroupMemberRequestDTO";
 
 import axios from "axios";
+import type { User } from "../models/User";
+import type { UserResponseDTO } from "../dto/UserResponseDTO";
+import { toUser } from "../mapper/user.mapper";
 
 
 const BASE_URL = "api/groups";
 
+// POST /api/groups/{groupId}/members
+// Add a user to a group
 export async function addUserToGroup(groupId: string,
                                     payload: AddGroupMemberRequestDTO,
                                     token: string): Promise<void>
@@ -21,5 +26,23 @@ export async function addUserToGroup(groupId: string,
     }
     catch (error: any)
     { throw new Error(error.response?.data?.message || error.message || "Failed to add member"); }
+}
+
+// GET /api/groups/{groupId}/members
+// Get the list of users in a group
+export async function getUserFromGroup(groupId: string, token: string): Promise<User[]>
+{
+    try
+    {
+        const { data } = await axios.get<UserResponseDTO[]>(`${BASE_URL}/${groupId}/members`,
+                                                            { headers: {
+                                                                        "Content-Type": "application/json",
+                                                                        Authorization: `Bearer ${token}`,
+                                                                    },
+                                                            });
+        return data.map(toUser);
+    }
+    catch (error: any)
+    { throw new Error(error.response?.data?.message || error.message || "Failed to fetch group members"); }
 }
                                     
