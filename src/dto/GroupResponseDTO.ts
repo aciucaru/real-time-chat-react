1369513e324df8ed3909b1e19190e7f219a7cfde
aciucaru <aciukru@gmail.com>
2025-11-29 17:57:09 +0200

    Removed "group'-related models and services

--- src/dto/GroupResponseDTO.ts
+++
@@ -1,7 +0,0 @@
-export interface GroupResponseDTO
-{
-    id: string; // UUID type on the back-end which will serialize to string in JSON format
-    name: string;
-    description: string;
-    ownerId: string;  // ID of the creator/admin, aslo UUID type on the back-end
-}
