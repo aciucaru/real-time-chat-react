0d6f41088d59146950611fe4190678d5a2ad2e52
aciucaru <aciukru@gmail.com>
2025-12-01 18:21:37 +0200

    Added comments

--- src/ui/components/NavBar.tsx
+++ src/ui/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 
+// This navigation bar is for testing purposes only. It will be removed in the final version.
 export function NavBar()
 {
     const navigate = useNavigate();
@@ -8,6 +9,7 @@ export function NavBar()
         <nav>
             <Link to="/login">Login</Link> |{' '}
             <Link to="/register">Register</Link> |{' '}
+            <Link to="/chat">Chat</Link> {' '}
         </nav>
     );
 }
