3b2370ccab7b048816430543a85db8400400eece
aciucaru <aciukru@gmail.com>
2025-12-01 19:52:46 +0200

    Added comments

--- src/services/auth/axios-clients.ts
+++ src/services/auth/axios-clients.ts
@@ -5,22 +5,38 @@ import { authState } from "./auth-state";
 const BASE_URL = "";
 const API_URL = BASE_URL + ":8080/payara.example-0.1-SNAPSHOT/resources";
 
+// Public axios client
 export const axiosPublicClient = axios.create({
                                             baseURL: import.meta.env.VITE_API_URL,
                                             headers: { "Content-Type": "application/json" },
                                         });
 
+
+// Axios client for authenticated requests.
+// This client will require a token to send with the request, but we do not store the token
+// inside the axios client directly, because that would be just the first version of the token.
 export const axiosAuthClient = axios.create({
                                             baseURL: import.meta.env.VITE_API_URL,
                                             headers: { "Content-Type": "application/json" },
                                         });
 
-// Attach latest access token dynamically
+// To make sure the axios client always gets the most recent token, we do not pass the token directly
+// to the axios client, but instead we use an interceptor.
+// This makes sure that, before every request, the axios client gets the most recent token.
+// This also works after logout, because the last token will be old/invalid and request will fail, as expected.
 axiosAuthClient.interceptors.request.use((config) =>
 {
     const token = authState.getToken();
+
+    // The 'config' object is an axios request configuration object.
+    // We use its 'headers' property, which also contains 'Authorization'.
+    // 'Authorization' is the standard HTTP header used by all servers to receive JWT
     if (token)
+        // Set the token on the HTTP headers, so that it's sent with the request
         config.headers.Authorization = `Bearer ${token}`;
+    else
+        // Delete the token if it's null
+        delete config.headers.Authorization;
     
     return config;
 });
