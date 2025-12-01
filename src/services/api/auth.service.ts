64461ba3530dcdfa59b475465b66223364f84c84
aciucaru <aciukru@gmail.com>
2025-12-01 17:30:42 +0200

    Corrected bad/missing types for sign-up and login functions

--- src/services/api/auth.service.ts
+++ src/services/api/auth.service.ts
@@ -5,6 +5,7 @@ import { toAuthResponse } from "../../mapper/auth.mapper";
 import axios from "axios";
 import type { AuthResponse } from "../../models/entity/AuthResponse";
 import { axiosPublicClient } from "../auth/axios-clients";
+import type { AuthResponseDTO } from "../../models/dto/AuthResponseDTO";
 
 
 const BASE_URL = "/auth";
@@ -13,10 +14,12 @@ const BASE_URL = "/auth";
 // Create a new user
 export async function signUp(payload: SignUpRequestDTO): Promise<AuthResponse>
 {
-    const response = await axiosPublicClient.post(`${BASE_URL}/register`,
-                                        payload,
-                                    { headers: {"Content-Type": "application/json",}, });
+    const response = await axiosPublicClient.post<AuthResponseDTO>(`${BASE_URL}/register`,
+                                                    payload,
+                                                    { headers: {"Content-Type": "application/json",}, }
+                                                );
 
+    // Convert from AuthResponseDTO to AuthResponse
     return toAuthResponse(response.data);
 }
 
@@ -24,7 +27,7 @@ export async function signUp(payload: SignUpRequestDTO): Promise<AuthResponse>
 // Authenticate and return a JWT/session token.
 export async function login(payload: LoginRequestDTO): Promise<AuthResponse>
 {
-    const response = await axiosPublicClient.post<AuthResponse>(`${BASE_URL}/login`,
+    const response = await axiosPublicClient.post<AuthResponseDTO>(`${BASE_URL}/login`,
                                 payload,
                                 { headers:
                                     {"Content-Type": "application/json",},
@@ -32,5 +35,6 @@ export async function login(payload: LoginRequestDTO): Promise<AuthResponse>
                                     withCredentials: true, 
                                 });
                                 
+    // Convert from AuthResponseDTO to AuthResponse
     return toAuthResponse(response.data);
 }
