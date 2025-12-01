95ea276e079c5c5869867a3bb32e401f36c072d3
aciucaru <aciukru@gmail.com>
2025-12-01 17:36:49 +0200

    Added comments

--- src/services/api/user.service.ts
+++ src/services/api/user.service.ts
@@ -3,6 +3,10 @@ import type { UserResponseDTO } from "../../models/dto/UserResponseDTO";
 
 const BASE_URL = "/api/users";
 
+// GET /api/users
+// GEt all users so that the current loged-in user can view the list of users an choose
+// a user to chat with.
+// Only a loged-in user is able to get the list of all users.
 export async function gatAllUsers(): Promise<UserResponseDTO[]>
 {
     const response = await axiosAuthClient.get<UserResponseDTO[]>(BASE_URL);
