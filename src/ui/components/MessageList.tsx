d8300f48aa719368bde881265b219b9abf08bcf4
aciucaru <aciukru@gmail.com>
2025-12-05 16:52:05 +0200

    Added HTML elements

--- src/ui/components/MessageList.tsx
+++ src/ui/components/MessageList.tsx
@@ -1,9 +1,10 @@
+import { useEffect, useRef } from "react";
 import type { MessageResponseDTO } from "../../models/dto/MessageResponseDTO";
 import { useAuthHook } from "../../services/auth/use-auth-hook";
 
 interface MessageListProps
 {
-    messages: MessageResponseDTO;
+    messages: MessageResponseDTO[]; // the array of messages
     loading: boolean;
     error: string | null;
 }
@@ -11,14 +12,49 @@ interface MessageListProps
 export default function MessageList(
     {
         messages,
-        laoding,
+        loading,
         error
     }: MessageListProps
 )
 {
     const { user } = useAuthHook();
+
+    // A reference to the div that represents the bottom message
     const bottomRef = useRef<HTMLDivElement | null>(null);
+
+    // React effect to automatically scroll to bottom whenever s new massage appears
+    useEffect( () =>
+        {
+            // Scroll to the <div>, with smooth scrolling
+            bottomRef.current?.scrollIntoView({behavior: "smooth"});
+        },
+        [messages] // run this effect every time 'isAuthenticated' changes
+    );
+
+    if (loading)
+        return <div>Loading messages...</div>
+
+    if (error)
+        return <div>{error}</div>
+
     return (
-        <div>List</div>
+    <div>
+        {messages.map( (msg) =>
+            {
+                const isMine = msg.senderId === user?.id;
+
+                // Here we convert (map) the message DTO to an HTML version of it
+                return (
+                    <div key={msg.id}>
+                        <div>{msg.content}</div>
+                        <div>{new Date(msg.timestamp).toLocaleTimeString()}</div>
+                    </div>
+                );
+            }
+        )}
+
+        {/* Invisible element used for auto-scroll */}
+        <div ref={bottomRef}></div>
+    </div>
     );
 }
