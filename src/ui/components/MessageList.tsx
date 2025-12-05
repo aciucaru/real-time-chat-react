aee4121207d261afc242ba55b40bb6cee301905d
aciucaru <aciukru@gmail.com>
2025-12-05 16:25:48 +0200

    Added props interface

--- src/ui/components/MessageList.tsx
+++ src/ui/components/MessageList.tsx
@@ -1,5 +1,23 @@
-export default function MessageList()
+import type { MessageResponseDTO } from "../../models/dto/MessageResponseDTO";
+import { useAuthHook } from "../../services/auth/use-auth-hook";
+
+interface MessageListProps
 {
+    messages: MessageResponseDTO;
+    loading: boolean;
+    error: string | null;
+}
+
+export default function MessageList(
+    {
+        messages,
+        laoding,
+        error
+    }: MessageListProps
+)
+{
+    const { user } = useAuthHook();
+    const bottomRef = useRef<HTMLDivElement | null>(null);
     return (
         <div>List</div>
     );
