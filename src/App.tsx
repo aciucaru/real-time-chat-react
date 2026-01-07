73fc910b6787c542439e1a8942e0fd2c6f55e3f0
aciucaru <aciukaru@gmail.com>
2026-01-07 19:58:02 +0200

    Added custom chat React provider

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,26 @@ import { NavBar } from './ui/components/NavBar';
 import LoginPage from './ui/pages/LoginPage';
 import RegisterPage from './ui/pages/RegisterPage';
 import ChatPage from './ui/pages/ChatPage';
+import { TopBar } from './ui/components/TopBar';
+import { ChatProvider } from './services/web-socket/Chat-Provider';
 
 function App()
 {
     return (
         <AuthProvider>
-            <BrowserRouter>
-                {/* No 'token' and 'setToken' props needed for the navigation bar, because it gets its
-                ** required props from the React 'AuthContext' automatically. */}
-                <NavBar></NavBar>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
-                    <Route path="/chat" element={<ChatPage />} />
-                </Routes>
-            </BrowserRouter>
+            <ChatProvider>
+                <BrowserRouter>
+                    <NavBar></NavBar>
+                    <TopBar></TopBar>
+                    {/* No 'token' and 'setToken' props needed for the navigation bar, because it gets its
+                    ** required props from the React 'AuthContext' automatically. */}
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/chat" element={<ChatPage />} />
+                    </Routes>
+                </BrowserRouter>
+            </ChatProvider>
         </AuthProvider>  
     );
 }
